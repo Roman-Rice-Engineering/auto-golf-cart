734b45eb1bc4453425b148820e5fe962ea0e2c04
Roman-Rice-Engineering <romanrice.engineering@gmail.com>
2024-05-20 10:42:36 -0400

    Added a code for setting up and writing to DAC

---
+++ bridge/firmware/bridge/dac.c
@@ -0,0 +1,11 @@
+#include "dac.h"
+#include <stdint.h>
+#include <Arduino.h>
+
+void dac_setup(void){
+	DDRB = 255;	
+}
+
+void dac_write(uint8_t val){
+	PORTB = val/4;
+}
---
+++ bridge/firmware/bridge/dac.h
@@ -0,0 +1,9 @@
+#ifndef DAC_H
+#define DAC_H
+
+#include <stdint.h>
+
+void dac_setup(void);
+void dac_write(uint8_t val);
+
+#endif
