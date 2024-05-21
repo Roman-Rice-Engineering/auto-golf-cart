6907c9beb6cd9579306dab7765dbcde983f4da8e
Roman-Rice-Engineering <romanrice.engineering@gmail.com>
2024-05-21 10:38:22 -0400

    Added dac_end function

--- bridge/firmware/bridge/dac.c
+++ bridge/firmware/bridge/dac.c
@@ -10,3 +10,7 @@ void dac_setup(void){
 void dac_write(uint8_t val){
 	PORTB = val/4;
 }
+
+void dac_end(void){
+  PORTB = 0;
+}
--- bridge/firmware/bridge/dac.h
+++ bridge/firmware/bridge/dac.h
@@ -5,5 +5,6 @@
 
 void dac_setup(void);
 void dac_write(uint8_t val);
+void dac_end(void);
 
 #endif
