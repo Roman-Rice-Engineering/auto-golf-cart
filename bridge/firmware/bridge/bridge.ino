08ab8548934ecf3bef95cff52ec30490bd0ad9b8
Roman-Rice-Engineering <romanrice.engineering@gmail.com>
2024-05-22 11:30:31 -0400

    Completed an untested version of v0.2 PCB comptible firmware

--- bridge/firmware/bridge/bridge.ino
+++ bridge/firmware/bridge/bridge.ino
@@ -1,5 +1,8 @@
 #include "config.h"
 #include "manual.h"
+#include "automatic.hpp"
+
+bool automatic = false;
 
 void setup() {
     pinMode(SWITCH_DRIVE_ENABLE_PIN, OUTPUT);
@@ -8,5 +11,21 @@ void setup() {
 }
 
 void loop() {
-  manual_loop();
+  bool new_automatic = digitalRead(MAN_AUTO_SELECT_PIN);
+
+  if(new_automatic != automatic){
+    automatic = new_automatic;
+    if(automatic == true){
+      manual_end();
+      automatic_setup(BAUD_RATE);
+    }else{
+      automatic_end();
+      manual_setup();
+    }
+  }
+  
+  if(automatic == false)
+    manual_loop();
+  else
+    automatic_loop();
 }
